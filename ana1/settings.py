6395cc386129499790bfe549a797bc42b412a6d3
Alexander J. Gray <grayalexanderj@gmail.com>
2020-04-19 21:18:57 -0400

    Changed DB to postgres, installed pycharm to edit from now on.

--- ana1/settings.py
+++ ana1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ana1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shoppinglist',
+        'USER': 'shoppinglistadmin',
+        'PASSWORD': 'D1ck123!',
+        'HOST': 'localhost',
+        'Port': '',
     }
 }
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
